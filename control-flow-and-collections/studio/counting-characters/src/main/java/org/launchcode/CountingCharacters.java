6fa119f67b8c098a6b8c60af3aaf9e0321ac1371
stephenmdevine <stephenmdevine@gmail.com>
2023-12-04 20:01:31 -0600

    Studio complete

--- control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/CountingCharacters.java
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/CountingCharacters.java
@@ -1,5 +1,8 @@
 package org.launchcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountingCharacters {
     public static void main(String[] args) {
         String phrase = "If the product of two terms is zero then common sense says " +
@@ -9,5 +12,16 @@ public class CountingCharacters {
                 "to equal zero. Once you’ve done that, it’s pretty straightforward " +
                 "from there.";
         char[] charactersInString = phrase.toCharArray();
+
+        HashMap<String, Integer> charactersCounted = new HashMap<>();
+        for (char i : charactersInString) {
+            String s = String.valueOf(i);
+            if (charactersCounted.containsKey(s)) {
+                charactersCounted.put(s, charactersCounted.get(s) + 1);
+            }   else {
+                charactersCounted.put(s, 1);
+            }
+        }
+        System.out.println(charactersCounted);
     }
 }
