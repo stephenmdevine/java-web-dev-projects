13f8de2e590d283413e49cce21b575d7a405342a
stephenmdevine <stephenmdevine@gmail.com>
2023-12-06 22:51:29 -0600

    Classes part 1 exercise completion

--- classes-part-one/exercises/src/main/java/Teacher.java
+++ classes-part-one/exercises/src/main/java/Teacher.java
@@ -3,4 +3,29 @@ public class Teacher {
     private String lastName;
     private String subject;
     private int yearsTeaching;
+
+    public String getFirstName() {
+        return firstName;
+    }
+    void setFirstName(String aFirstName) {
+        firstName = aFirstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    void setLastName(String aLastName) {
+        lastName = aLastName;
+    }
+    public String getSubject() {
+        return subject;
+    }
+    public void setSubject(String aSubject) {
+        subject = aSubject;
+    }
+    public int getYearsTeaching() {
+        return yearsTeaching;
+    }
+    public void setYearsTeaching(int aYearsTeaching) {
+        yearsTeaching = aYearsTeaching;
+    }
 }
